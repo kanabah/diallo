b4853ae3f1f7ab89e1d5eb822df812fd5015650d
kana bah <kanabah55@gmail.com>
2020-02-29 18:04:45 +0000

    Feat(Api to https and add key ssl in API)

--- api/server.js
+++ api/server.js
@@ -2,8 +2,13 @@ const express = require('express'),
 path = require('path'),
 bodyParser = require('body-parser'),
 cors = require('cors'),
+https = require('https'),
+fs = require('fs'),
 mongoose = require('mongoose'),
 config = require('./DB');
+
+var privateKey  = fs.readFileSync('/home/abfgrup/ssl/keys/9cdf0_fe081_15ba3e0fb174329e560b42255e27bd91.key', 'utf8');
+var certificate = fs.readFileSync('/home/abfgrup/ssl/certs/abf_grup_com_9cdf0_fe081_1614341213_2ed9e09cef0601506f76e0f193e9949b.crt', 'utf8');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '')));
@@ -31,6 +36,11 @@ app.use('/clients', clientRoutes);
 
 const port = process.env.PORT || 5202;
 
-const server = app.listen(port, function(){
+const options = {
+  key: privateKey,
+  cert: certificate
+};
+
+https.createServer(options, app).listen(port, function(){
   console.log('Listening on port ' + port);
 });
--- src/app/services/backend.ts
+++ src/app/services/backend.ts
@@ -1,2 +1,2 @@
-export let baseUrl = `http://204.93.169.49:5202/`
+export let baseUrl = `https://204.93.169.49:5202/`
 // export let baseUrl = `http://204.93.178.180:4001/`
