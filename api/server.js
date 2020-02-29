a43f7ba373e9978cfa22ee940d60a8e092463728
kana bah <kanabah55@gmail.com>
2020-02-29 12:05:03 +0000

    Feat(fix port 5202 for API)

--- api/server.js
+++ api/server.js
@@ -8,29 +8,29 @@ config = require('./DB');
 const app = express();
 app.use(express.static(path.join(__dirname, '')));
 // app.use(express.static(__dirname+'/images'));
-    // var logger = require('morgan');  
-    // var cookieParser = require('cookie-parser');
-    var passport = require('passport');
+// var logger = require('morgan');  
+// var cookieParser = require('cookie-parser');
+var passport = require('passport');
 
-    //======================START ROUTES================
-    const userRoutes = require('./routes/user.route');
-    const clientRoutes = require('./routes/client.route');
-    //======================END ROUTES================
+//======================START ROUTES================
+const userRoutes = require('./routes/user.route');
+const clientRoutes = require('./routes/client.route');
+//======================END ROUTES================
 
-    mongoose.Promise = global.Promise;
-    mongoose.connect(config.DBDEV, { useNewUrlParser: true, useUnifiedTopology: true }).then(
-      () => {console.log('Database is connected') },
-      err => { console.log('Can not connect to the database'+ err)}
-    );
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DBDEV, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+  () => console.log('Database is connected'),
+  err => console.log('Can not connect to the database'+ err)
+);
 
-    app.use(bodyParser.json());
-    app.use(cors());
+app.use(bodyParser.json());
+app.use(cors());
 
-    app.use('/users', userRoutes);
-    app.use('/clients', clientRoutes);
+app.use('/users', userRoutes);
+app.use('/clients', clientRoutes);
 
-    const port = process.env.PORT || 4001;
+const port = process.env.PORT || 5202;
 
-    const server = app.listen(port, function(){
-     console.log('Listening on port ' + port);
-    });
+const server = app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
